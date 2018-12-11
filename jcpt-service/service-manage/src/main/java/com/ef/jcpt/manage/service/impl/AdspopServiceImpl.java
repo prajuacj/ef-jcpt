2bf075accdaf173b2532bb43181b5e038c760033
prajuacj <prajuacj@163.com>
2018-12-11 10:27:47 +0800

    添加根据用户查询个人发布的广告信息

--- jcpt-service/service-manage/src/main/java/com/ef/jcpt/manage/service/impl/AdspopServiceImpl.java
+++ jcpt-service/service-manage/src/main/java/com/ef/jcpt/manage/service/impl/AdspopServiceImpl.java
@@ -242,9 +242,9 @@ public class AdspopServiceImpl implements IAdspopService {
 		// TODO Auto-generated method stub
 		BasicServiceModel<String> bsm = new BasicServiceModel<String>();
 		long curTime = System.currentTimeMillis();
-		
-		if(null==popadsIds) {
-			popadsIds=new int[0];
+
+		if (null == popadsIds) {
+			popadsIds = new int[0];
 		}
 
 		List<Integer> list = new ArrayList<Integer>();
@@ -379,14 +379,14 @@ public class AdspopServiceImpl implements IAdspopService {
 	}
 
 	@Override
-	public int countPopadsByPage(String taskName, String taskStatus, int modelId) {
+	public int countPopadsByPage(String taskName, String taskStatus, int modelId, String publishUser) {
 		// TODO Auto-generated method stub
-		return popadsInfoMapper.countPopadsByPage(taskName, taskStatus, modelId);
+		return popadsInfoMapper.countPopadsByPage(taskName, taskStatus, modelId, publishUser);
 	}
 
 	@Override
-	public BasicServiceModel<String> listPopadsByPage(String taskName, String taskStatus, int modelId, int pageIndex,
-			int pageSize) {
+	public BasicServiceModel<String> listPopadsByPage(String taskName, String taskStatus, int modelId,
+			String publishUser, int pageIndex, int pageSize) {
 		// TODO Auto-generated method stub
 		BasicServiceModel<String> bsm = new BasicServiceModel<String>();
 		// TODO Auto-generated method stub
@@ -394,7 +394,8 @@ public class AdspopServiceImpl implements IAdspopService {
 		if (start < 0) {
 			start = 0;
 		}
-		List<PopadsInfo> list = popadsInfoMapper.listPopadsByPage(taskName, taskStatus, modelId, start, pageSize);
+		List<PopadsInfo> list = popadsInfoMapper.listPopadsByPage(taskName, taskStatus, modelId, publishUser, start,
+				pageSize);
 		if (null != list) {
 			JSONObject data = new JSONObject();
 			data.put("data", list);
